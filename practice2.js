20d52a5189b3098bf55745be299f1147c79bf0f5
Ahfaz90 <ahfazahmedkhan.khan@gmail.com>
2022-10-11 00:29:47 +0530

    files included

---
+++ practice2.js
@@ -0,0 +1,7 @@
+function myfunction()
+        {
+            
+            document.getElementById("demo2").innerHTML = "My First JavaScript on click event";
+            document.getElementById("demo2").style.background = "red";
+            document.getElementById("demo2").style.fontSize = "100px";
+        }
